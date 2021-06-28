ea593d92557f258614494fd7451feede7a6c9c80
glaucoserrano <glaucoserrano@gmail.com>
2021-06-28 19:49:38 -0300

    First Commit

---
+++ ListaArtilheiros/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListaArtilheiros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
